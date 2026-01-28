b2078cdc56e506547cd4c2fffd185b0138d4b1fe
Lâm Trần (Kaa) <123573848+LamTran0511@users.noreply.github.com>
2026-01-29 00:29:33 +0700

    Add files via upload

---
+++ HomeWork_IOC_BE/SS9/LT1_SS9/Main.java
@@ -0,0 +1,19 @@
+package SS9.LT1_SS9;
+
+import SS9.LT1_SS9.Rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        SS9.LT1_SS9.Rectangle r1 = new SS9.LT1_SS9.Rectangle(3,4);
+        SS9.LT1_SS9.Rectangle r2 = new SS9.LT1_SS9.Rectangle(5, 2);
+        SS9.LT1_SS9.Rectangle r3 = new Rectangle(4.5, 3.5);
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+    }
+}
