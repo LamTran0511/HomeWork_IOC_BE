61eeaec5dd10eb11d070f23686667e2b1badb934
Lâm Trần (Kaa) <123573848+LamTran0511@users.noreply.github.com>
2026-01-28 19:37:35 +0700

    Add files via upload

---
+++ HomeWork_IOC_BE/SS10/BaiTap02_SS10/Circle.java
@@ -0,0 +1,13 @@
+package SS10.BaiTap02_SS10;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ HomeWork_IOC_BE/SS10/BaiTap02_SS10/Shape.java
@@ -0,0 +1,7 @@
+package SS10.BaiTap02_SS10;
+
+public class Shape {
+    public double area() {
+        return 0;
+    }
+}
